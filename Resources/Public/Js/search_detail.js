850dc3cec6972913fb12a96c6cf56e62d53fd298
Thomas Juhnke <tommy@van-tomas.de>
2012-03-28 22:27:49 +0200

    [BUGFIX] adding event delegation for subdivisions select list
    As the select list is rebuild completely (for Internet Explorer
    compatibility issues) and updated in a surrounding container element,
    the plugin needs some event delegation mapping for the .on() method.

--- Resources/Public/Js/search_detail.js
+++ Resources/Public/Js/search_detail.js
@@ -1,6 +1,89 @@
 (function ($) {
 	"use strict";
 
+	var
+		settings = {
+			// parameter prefix, corresponds with extension plugin name
+			paramPrefix: 'tx_justimmo_geofilterupdatexhr',
+			// parameter key which should be set for the update action
+			paramKey: '',
+			// array of objects with url & callback properties set, defining which url should trigger which update action 
+			actions: [],
+			// prefix for created filter form fields
+			filterFieldPrefix: 'tx_justimmo_searchresults'
+		},
+	
+		methods = {
+			/**
+			 * creates an option item for <select> lists
+			 *
+			 * @param integer key
+			 * @param object value
+			 * @returns {String} the resulting <option> tag
+			 */
+			buildOptionItem: function (key, value) {
+				return '<option value="' + value.name + '">' + value.name + '</option>';
+			},
+	
+			/**
+			 * creates a checkbox item
+			 * @param integer key
+			 * @param object value
+			 * @returns {String} the resulting checkbox markup
+			 */
+			buildCheckboxItem: function (key, value) {
+				var
+					checkboxHidden = '<input type="hidden" value="" name="' + settings.filterFieldPrefix + '[filter][region]" />',
+					checkbox = '<input id="detail_region_' + value.id + '" type="checkbox" value="' + value.id + '" name="' + settings.filterFieldPrefix + '[filter][region][]">',
+					label = '<label for="detail_region_' + value.id + '">' + value.name + '</label>';
+	
+				return checkboxHidden + checkbox + label;
+			},
+	
+			/**
+			 * updates the sub divisions filter element
+			 *
+			 * @param data incoming API response data as a JSON object
+			 * @returns {Boolean} false if data.bundesland element couldn't be found
+			 */
+			updateSubDivisions: function (data) {
+				var
+					items = [];
+	
+				if ('undefined' === typeof data.bundesland) {
+					return false;
+				}
+	
+				items.push('<option value=""></option>');
+				$.each(data.bundesland, function (key, val) {
+					items.push(methods.buildOptionItem(key, val));
+				});
+	
+				$('#bundeslandId-container').html('<select id="detail_bundesland_id" name="' + settings.filterFieldPrefix + '[filter][bundeslandId]">' + items.join('') + '</select>');
+			},
+	
+			/**
+			 * updates the region filter elements (checkboxes)
+			 * 
+			 * @param data incoming API response data as a JSON object
+			 * @returns {Boolean} false if data.region element couldn't be found
+			 */
+			updateRegions: function (data) {
+				var
+					items = [];
+	
+				if ('undefined' === typeof data.region) {
+					return false;
+				}
+	
+				$.each(data.region, function (key, val) {
+					items.push(methods.buildCheckboxItem(key, val));
+				});
+	
+				$('#detail_regions').html('<li>' + items.join('</li><li>') + '</li>');
+			}
+		};
+
 	/**
 	 * geographical filter form elements update plugin
 	 *
@@ -11,95 +94,16 @@
 	 * (c) 2012 B&G Consulting & Commerce GmbH <office@bgcc.at>
 	 *
 	 * @author Thomas Juhnke <tommy@van-tomas.de>
+	 *
+	 * @param 
 	 */
-	$.fn.GeoFilterUpdate = function (options) {
-		var
-			settings = $.extend({
-				// parameter prefix, corresponds with extension plugin name
-				paramPrefix: 'tx_justimmo_geofilterupdatexhr',
-				// parameter key which should be set for the update action
-				paramKey: '',
-				// array of objects with url & callback properties set, defining which url should trigger which update action 
-				actions: [],
-				// prefix for created filter form fields
-				filterFieldPrefix: 'tx_justimmo_searchresults'
-			}, options),
-
-			methods = {
-				/**
-				 * creates an option item for <select> lists
-				 *
-				 * @param integer key
-				 * @param object value
-				 * @returns {String} the resulting <option> tag
-				 */
-				buildOptionItem: function (key, value) {
-					return '<option value="' + value.name + '">' + value.name + '</option>';
-				},
-
-				/**
-				 * creates a checkbox item
-				 * @param integer key
-				 * @param object value
-				 * @returns {String} the resulting checkbox markup
-				 */
-				buildCheckboxItem: function (key, value) {
-					var
-						checkboxHidden = '<input type="hidden" value="" name="' + settings.filterFieldPrefix + '[filter][region]" />',
-						checkbox = '<input id="detail_region_' + value.id + '" type="checkbox" value="' + value.id + '" name="' + settings.filterFieldPrefix + '[filter][region][]">',
-						label = '<label for="detail_region_' + value.id + '">' + value.name + '</label>';
-
-					return checkboxHidden + checkbox + label;
-				},
-
-				/**
-				 * updates the sub divisions filter element
-				 *
-				 * @param data incoming API response data as a JSON object
-				 * @returns {Boolean} false if data.bundesland element couldn't be found
-				 */
-				updateSubDivisions: function (data) {
-					var
-						items = [];
-
-					if ('undefined' === typeof data.bundesland) {
-						return false;
-					}
-
-					items.push('<option value=""></option>');
-					$.each(data.bundesland, function (key, val) {
-						items.push(methods.buildOptionItem(key, val));
-					});
-
-					$('#bundeslandId-container').html('<select id="detail_bundesland_id" name="' + settings.filterFieldPrefix + '[filter][bundeslandId]">' + items.join('') + '</select>');
-				},
-
-				/**
-				 * updates the region filter elements (checkboxes)
-				 * 
-				 * @param data incoming API response data as a JSON object
-				 * @returns {Boolean} false if data.region element couldn't be found
-				 */
-				updateRegions: function (data) {
-					var
-						items = [];
-
-					if ('undefined' === typeof data.region) {
-						return false;
-					}
-
-					$.each(data.region, function (key, val) {
-						items.push(methods.buildCheckboxItem(key, val));
-					});
-
-					$('#detail_regions').html('<li>' + items.join('</li><li>') + '</li>');
-				}
-			};
+	$.fn.GeoFilterUpdate = function (options, selector) {
+		settings = $.extend(settings, options);
 
 		/**
 		 * binds the change event listener to the incoming jQuery object
 		 */
-		this.change(function (event) {
+		this.on('change', selector, function (event) {
 			var
 				// field value of bound HTML node
 				fieldValue = $(this).val();
